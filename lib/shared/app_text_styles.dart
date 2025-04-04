0e0fc32825908f27915aec5b34048d08c51aac09
Nofa Nisrina <nofanisrina@gmail.com>
2025-04-05 03:15:59 +0700

    custom textstyle fix

--- lib/shared/app_text_styles.dart
+++ lib/shared/app_text_styles.dart
@@ -3,120 +3,137 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextStyles {
 
+  // Silent Notes:
+  /* 
+    Karena pada custom text style ini aku ingin mengimplementasi .sp dari screen util pada deklarasinya,
+    Aku membuat tiap variabel menjadi get method, karena jika mengimplementasi pakai
+    
+    static 'const' namaVariabel = TextStyle()
+
+    si screenutil itu tidak bisa pakai constant, jadi dia butuh pakai 'final'
+    tapi, bakal muncul masalah baru
+    ketika kita pakai final, mungkin ketika debug itu gpp gusy
+    tapii, ketika kira lepas mode debug (mode release)
+    screenutilnya tuh bkal error gusy
+    dan UI nya ga muncul omegat
+    Jadi kaya silent error gituu
+
+    Sekian, cmiiw
+  */
+
   // Font Family
-  static final _fontFamily = 'Inter';
+  static const _fontFamily = 'Inter';
   
   // H1 (30)
-  static final h1Bold = TextStyle(
-    fontSize: 30.sp,
+  static TextStyle get h1Bold => TextStyle(
+    fontSize: 30,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
-    
   );
 
-  static final h1SemiBold = TextStyle(
-    fontSize: 30.sp,
+  static TextStyle get h1SemiBold => TextStyle(
+    fontSize: 30,
     fontWeight: FontWeight.w600,
     fontFamily: _fontFamily
   );
 
-  static final h1Regular = TextStyle(
-    fontSize: 30.sp,
+  static TextStyle get h1Regular => TextStyle(
+    fontSize: 30,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily
   );
 
   // H2 (25)
-    static final h2Bold = TextStyle(
-      fontSize: 25.sp,
+    static TextStyle get h2Bold => TextStyle(
+      fontSize: 25,
       fontWeight: FontWeight.w700,
       fontFamily: _fontFamily
     );
 
-    static final h2Semibold = TextStyle(
-      fontSize: 25.sp,
+    static TextStyle get h2Semibold => TextStyle(
+      fontSize: 25,
       fontWeight: FontWeight.w600,
       fontFamily: _fontFamily
     );
 
-    static final h2Regular = TextStyle(
-      fontSize: 25.sp,
+    static TextStyle get h2Regular => TextStyle(
+      fontSize: 25,
       fontWeight: FontWeight.w400,
       fontFamily: _fontFamily
     );
 
     //Medium (20)
-    static final mediumBold = TextStyle(
-      fontSize: 20.sp,
+    static TextStyle get mediumBold => TextStyle(
+      fontSize: 20,
       fontWeight: FontWeight.w700,
       fontFamily: _fontFamily,
     );
 
-    static final mediumSemiBold = TextStyle(
-      fontSize: 20.sp,
+    static TextStyle get mediumSemiBold => TextStyle(
+      fontSize: 20,
       fontWeight: FontWeight.w600,
       fontFamily: _fontFamily,
     );
 
-    static final mediumRegular = TextStyle(
-      fontSize: 20.sp,
+    static TextStyle get mediumRegular => TextStyle(
+      fontSize: 20,
       fontWeight: FontWeight.w400,
       fontFamily: _fontFamily,
     );
 
     // Base (16)
-    static final baseBold = TextStyle(
-      fontSize: 16.sp,
+    static TextStyle get baseBold => TextStyle(
+      fontSize: 16,
       fontWeight: FontWeight.w700,
       fontFamily: _fontFamily,
     );
 
-    static final baseSemiBold = TextStyle(
-      fontSize: 16.sp,
+    static TextStyle get baseSemiBold => TextStyle(
+      fontSize: 16,
       fontWeight: FontWeight.w600,
       fontFamily: _fontFamily,
     );
 
-    static final baseRegular = TextStyle(
+    static TextStyle get baseRegular => TextStyle(
       fontSize: 16.sp,
       fontWeight: FontWeight.w400,
       fontFamily: _fontFamily,
     );
 
     // Small (12)
-    static final smallBold = TextStyle(
-      fontSize: 12.sp,
+    static TextStyle get smallBold => TextStyle(
+      fontSize: 12,
       fontWeight: FontWeight.w700,
       fontFamily: _fontFamily,
     );
 
-    static final smallSemiBold = TextStyle(
-      fontSize: 12.sp,
+    static TextStyle get smallSemiBold => TextStyle(
+      fontSize: 12,
       fontWeight: FontWeight.w600,
       fontFamily: _fontFamily,
     );
 
-    static final smallRegular = TextStyle(
-      fontSize: 12.sp,
+    static TextStyle get smallRegular => TextStyle(
+      fontSize: 12,
       fontWeight: FontWeight.w400,
       fontFamily: _fontFamily,
     );
 
     // XS (10)
-    static final xsBold = TextStyle(
-      fontSize: 10.sp,
+    static TextStyle get xsBold => TextStyle(
+      fontSize: 10,
       fontWeight: FontWeight.w700,
       fontFamily: _fontFamily,
     );
 
-    static final xsSemiBold = TextStyle(
-      fontSize: 10.sp,
+    static TextStyle get xsSemiBold => TextStyle(
+      fontSize: 10,
       fontWeight: FontWeight.w600,
       fontFamily: _fontFamily,
     );
 
-    static final xsRegular = TextStyle(
-      fontSize: 10.sp,
+    static TextStyle get xsRegular => TextStyle(
+      fontSize: 10,
       fontWeight: FontWeight.w400,
       fontFamily: _fontFamily,
     );
