724ed4f5415f49f067157908b3150d534dcfa603
Nofa Nisrina <nofanisrina@gmail.com>
2025-04-06 23:00:28 +0700

    login page progress

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_basic_notes/shared/app_text_styles.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget{
+  
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(fontSize: 24.sp),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            SizedBox(height: 130.h,),
+
+            Text(
+              "Daftar",
+              style: AppTextStyles.h2Bold,
+            ),
+
+            SizedBox(height: 4.h,),
+
+            Text(
+              "Buat akun baru kamu!",
+              style: AppTextStyles.baseRegular,
+            ),
+
+            SizedBox(height: 60.h,),
+
+            
+
+
+          ],
         ),
       ),
     );
   }
+
 }
