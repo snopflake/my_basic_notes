a4d6ccb2b945840d6495d47d6730f9275464acb1
Nofa Nisrina <nofanisrina@gmail.com>
2025-04-07 00:51:11 +0700

    custom input field progress

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_basic_notes/shared/app_text_styles.dart';
+import 'package:my_basic_notes/shared/widgets/input_field.dart';
 
 class LoginPage extends StatefulWidget{
   
@@ -10,42 +12,84 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage> {
 
+  //Controller
+  final TextEditingController namaLengkapController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController kataSandiController = TextEditingController();
+  final TextEditingController konfirmasiKataSandiController = TextEditingController();
+
+  //Dispose
+  @override
+  void dispose() {
+    namaLengkapController.dispose();
+    usernameController.dispose();
+    kataSandiController.dispose();
+    konfirmasiKataSandiController.dispose();
+    super.dispose();
+  }
+  
+  //Build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-            SizedBox(height: 130.h,),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 40.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              SizedBox(height: 130.h,),
+          
+              Text(
+                "Daftar",
+                style: AppTextStyles.h2Bold,
+              ),
+          
+              SizedBox(height: 4.h,),
+          
+              Text(
+                "Buat akun baru kamu!",
+                style: AppTextStyles.baseRegular,
+              ),
+          
+              SizedBox(height: 60.h,),
+          
+              AppTextField(
+                label: "Nama Lengkap", 
+                controller: namaLengkapController
+              ),
+          
+              SizedBox(height: 24.h,),
+          
+              AppTextField(
+                label: "Username", 
+                controller: usernameController,
+              ),
 
-            Text(
-              "Daftar",
-              style: AppTextStyles.h2Bold,
-            ),
+              SizedBox(height: 24.h,),
 
-            SizedBox(height: 4.h,),
+              AppTextField(
+                label: "Kata Sandi", 
+                controller: kataSandiController,
+                isPassword: true,
+              ),
 
-            Text(
-              "Buat akun baru kamu!",
-              style: AppTextStyles.baseRegular,
-            ),
+              SizedBox(height: 24.h,),
 
-            SizedBox(height: 60.h,),
-
-            Text(
-              "Nama Lengkap",
-              style: AppTextStyles.baseRegular,
-            ),
-
-            SizedBox(height: 12.h,),
-
-                       
+              AppTextField(
+                label: "Konfirmasi Kata Sandi", 
+                controller: konfirmasiKataSandiController,
+                isPassword: true,
+              ),
 
 
-
-          ],
+                         
+          
+          
+          
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/shared/widgets/input_field.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_basic_notes/shared/app_colors.dart';
+import 'package:my_basic_notes/shared/app_text_styles.dart';
+
+class AppTextField extends StatefulWidget {
+
+  final String label;
+  final TextEditingController controller;
+  final bool isPassword;
+  final TextInputType keyboardType;
+
+  const AppTextField({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text
+  });
+  
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+        Text(
+          widget.label, 
+          style: AppTextStyles.baseRegular
+        ),
+
+        SizedBox(height: 12.h),
+
+        SizedBox(
+          height: 40.h,
+          child: TextField(
+            controller: widget.controller,
+            keyboardType: widget.keyboardType,
+            obscureText: widget.isPassword ? _obscureText : false,
+            style: AppTextStyles.baseRegular.copyWith(color: AppColors.blue800),
+            
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.w.h),
+              hintText: "Masukkan ${widget.label.toLowerCase()}",
+              hintStyle: AppTextStyles.baseRegular.copyWith(color: AppColors.blue700),
+              fillColor: AppColors.blue50,
+              filled: true,
+          
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.dg),
+                borderSide: BorderSide(
+                  color: AppColors.blue900,
+                  width: 1.w,
+                )
+              ),
+          
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.dg),
+                borderSide: BorderSide(
+                  color: AppColors.orange500,
+                  width: 1.5.w,
+                )
+              ),
+              
+              suffixIcon: widget.isPassword ? 
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  }, 
+                  icon: Icon(
+                    _obscureText ? Icons.visibility_off : Icons.visibility
+                  ))
+                  : null,
+            ),
+          ),
+        ),  
+      ],
+    );
+  }
+}
