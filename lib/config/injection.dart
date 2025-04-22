c0cfc1c1453f68e121d746c7202b6bc3603fc1fb
Nofa Nisrina <nofanisrina@gmail.com>
2025-04-22 16:18:13 +0700

    refactor: auth_repository_impl to data/repositories

--- lib/config/injection.dart
+++ lib/config/injection.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:my_basic_notes/features/auth/domain/repositories/auth_repository.dart';
-import 'package:my_basic_notes/features/auth/domain/repositories/auth_repository_impl.dart';
+import 'package:my_basic_notes/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:my_basic_notes/features/auth/domain/usecases/login_usecase.dart';
 import 'package:my_basic_notes/features/auth/domain/usecases/signup_usecase.dart';
 import 'package:my_basic_notes/features/auth/presentation/bloc/auth_bloc.dart';
--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'auth_repository.dart';
+import '../../domain/repositories/auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
