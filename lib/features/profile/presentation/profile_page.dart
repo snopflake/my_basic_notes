6e7b6f7d5112e26966764351777c4549f144bcf1
Nofa Nisrina <nofanisrina@gmail.com>
2025-05-08 16:29:27 +0700

    feat: profile

---
+++ lib/features/profile/presentation/profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 24.sp),
+        ),
+      ),
+    );
+  }
+}
