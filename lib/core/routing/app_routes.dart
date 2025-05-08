baea359921605625be267f1824167f73f1c10062
Nofa Nisrina <nofanisrina@gmail.com>
2025-05-09 00:17:53 +0700

    chore (routing): add centralized route names

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const splash = '/';
+  static const login = '/login';
+  static const signup = '/signup';
+  static const main = '/main';
+  static const home = '/home';
+  static const addnotes = '/addnotes';
+  static const profile = '/profile';
+}
